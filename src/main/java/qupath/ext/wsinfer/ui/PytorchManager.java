dccf38e39f560be07bc40d8f222e5b388f90e30c
Pete <pete.bankhead@gmail.com>
2023-11-03 18:47:56 +0000

    Update version

--- src/main/java/qupath/ext/wsinfer/ui/PytorchManager.java
+++ src/main/java/qupath/ext/wsinfer/ui/PytorchManager.java
@@ -42,6 +42,7 @@ class PytorchManager {
         boolean includesMPS = false; // Don't add MPS twice
         var engine = getEngineOffline();
         if (engine != null) {
+            // This is expected to return GPUs if available, or CPU otherwise
             for (var device : engine.getDevices()) {
                 String name = device.getDeviceType();
                 availableDevices.add(name);
